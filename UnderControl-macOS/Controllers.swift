dfc1e0372a53a69667a24792ac7352323fd31d7e
Dariusz Rybicki <darrarski@gmail.com>
2017-05-16 00:41:49 +0200

    Implement controller connection observing

--- UnderControl-macOS/Controllers.swift
+++ UnderControl-macOS/Controllers.swift
@@ -1,7 +1,61 @@
+import Foundation
+import GameController
+import RxSwift
+
 public class Controllers {
 
-    public init() {
+    public init(notificationCenter: NotificationCenter = NotificationCenter.default) {
+        self.notificationCenter = notificationCenter
+        setupControllerConnectionObservers()
+    }
 
+    deinit {
+        removeControllerConnectionObservers()
+    }
+
+    private let notificationCenter: NotificationCenter
+
+    // MARK: Controller connection observing
+
+    var controllerDidConnect: Observable<ControllerType> {
+        return controllerDidConnectSubject.asObservable()
+    }
+
+    var controllerDidDisconnect: Observable<ControllerType> {
+        return controllerDidDisconnectSubject.asObservable()
+    }
+
+    private let controllerDidConnectSubject = PublishSubject<ControllerType>()
+    private let controllerDidDisconnectSubject = PublishSubject<ControllerType>()
+    private var controllerDidConnectObserver: Any?
+    private var controllerDidDisconnectObserver: Any?
+
+    private func setupControllerConnectionObservers() {
+        controllerDidConnectObserver = notificationCenter.addObserver(
+            forName: Notification.Name.GCControllerDidConnect,
+            object: nil,
+            queue: OperationQueue.main,
+            using: { [weak self] in
+                guard let controller = $0.object as? ControllerType else { return }
+                self?.controllerDidConnectSubject.onNext(controller)
+            }
+        )
+        controllerDidDisconnectObserver = notificationCenter.addObserver(
+            forName: Notification.Name.GCControllerDidDisconnect,
+            object: nil,
+            queue: OperationQueue.main,
+            using: { [weak self] in
+                guard let controller = $0.object as? ControllerType else { return }
+                self?.controllerDidDisconnectSubject.onNext(controller)
+            }
+        )
+    }
+
+    private func removeControllerConnectionObservers() {
+        let observers = [controllerDidConnectObserver, controllerDidDisconnectObserver].flatMap { $0 }
+        observers.forEach { notificationCenter.removeObserver($0) }
+        controllerDidConnectObserver = nil
+        controllerDidDisconnectObserver = nil
     }
 
 }
--- UnderControlTests-macOS/Specs/ControllersSpec.swift
+++ UnderControlTests-macOS/Specs/ControllersSpec.swift
@@ -1,5 +1,8 @@
 import Quick
 import Nimble
+import RxTest
+import Foundation
+import GameController
 @testable import UnderControl
 
 class ControllersSpec: QuickSpec {
@@ -7,13 +10,108 @@ class ControllersSpec: QuickSpec {
     override func spec() {
         describe("Controllers") {
             var controllers: Controllers!
+            var notificationCenter: NotificationCenter!
 
             beforeEach {
-                controllers = Controllers()
+                notificationCenter = NotificationCenter()
+                controllers = Controllers(notificationCenter: notificationCenter)
             }
 
             afterEach {
                 controllers = nil
+                notificationCenter = nil
+            }
+
+            describe("controller") {
+                var controller: ControllerDouble!
+
+                beforeEach {
+                    controller = ControllerDouble()
+                }
+
+                afterEach {
+                    controller = nil
+                }
+
+                context("did connect") {
+                    var scheduler: TestScheduler!
+                    var observer: TestableObserver<ControllerType>!
+
+                    beforeEach {
+                        scheduler = TestScheduler(initialClock: 0)
+                        observer = scheduler.createObserver(ControllerType.self)
+                        _ = controllers.controllerDidConnect.subscribe(observer)
+                        notificationCenter.post(name: Notification.Name.GCControllerDidConnect, object: controller)
+                    }
+
+                    it("should produce one event") {
+                        expect(observer.events.count).to(equal(1))
+                    }
+
+                    it("should produce correct event") {
+                        expect(observer.events.first?.value.element).to(be(controller))
+                    }
+                }
+
+                context("did disconnect") {
+                    var scheduler: TestScheduler!
+                    var observer: TestableObserver<ControllerType>!
+
+                    beforeEach {
+                        scheduler = TestScheduler(initialClock: 0)
+                        observer = scheduler.createObserver(ControllerType.self)
+                        _ = controllers.controllerDidDisconnect.subscribe(observer)
+                        notificationCenter.post(name: Notification.Name.GCControllerDidDisconnect, object: controller)
+                    }
+
+                    it("should produce one event") {
+                        expect(observer.events.count).to(equal(1))
+                    }
+
+                    it("should produce correct event") {
+                        expect(observer.events.first?.value.element).to(be(controller))
+                    }
+                }
+            }
+
+            describe("invalid controller") {
+                context("did connect") {
+                    var scheduler: TestScheduler!
+                    var observer: TestableObserver<ControllerType>!
+
+                    beforeEach {
+                        scheduler = TestScheduler(initialClock: 0)
+                        observer = scheduler.createObserver(ControllerType.self)
+                        _ = controllers.controllerDidConnect.subscribe(observer)
+                        notificationCenter.post(
+                            name: Notification.Name.GCControllerDidConnect,
+                            object: "not a controller"
+                        )
+                    }
+
+                    it("should produce no events") {
+                        expect(observer.events).to(beEmpty())
+                    }
+                }
+
+                context("did disconnect") {
+                    var scheduler: TestScheduler!
+                    var observer: TestableObserver<ControllerType>!
+
+                    beforeEach {
+                        scheduler = TestScheduler(initialClock: 0)
+                        observer = scheduler.createObserver(ControllerType.self)
+                        _ = controllers.controllerDidDisconnect.subscribe(observer)
+                        notificationCenter.post(
+                            name: Notification.Name.GCControllerDidDisconnect,
+                            object: "not a controller"
+                        )
+                    }
+
+                    it("should produce no events") {
+                        expect(observer.events).to(beEmpty())
+                    }
+                }
             }
         }
     }
