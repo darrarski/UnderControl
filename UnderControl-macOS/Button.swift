9e93aa63811880eb50ff33e644715421e32bd42b
Dariusz Rybicki <darrarski@gmail.com>
2017-05-17 00:47:49 +0200

    Add pressedChanged and valueChanged observables to Button

--- UnderControl-macOS/Button.swift
+++ UnderControl-macOS/Button.swift
@@ -1,11 +1,33 @@
 import GameController
+import RxSwift
 
 public class Button {
 
     public init(gcButton: GCControllerButtonInput) {
         self.gcButton = gcButton
+        setupHandlers()
     }
 
     public let gcButton: GCControllerButtonInput
+
+    public var pressedChanged: Observable<Bool> {
+        return pressedChangedSubject.asObservable()
+    }
+
+    public var valueChanged: Observable<Float> {
+        return valueChangedSubject.asObservable()
+    }
+
+    private let pressedChangedSubject = PublishSubject<Bool>()
+    private let valueChangedSubject = PublishSubject<Float>()
+
+    private func setupHandlers() {
+        gcButton.pressedChangedHandler = { [weak self] (_, _, isPressed) in
+            self?.pressedChangedSubject.onNext(isPressed)
+        }
+        gcButton.valueChangedHandler = { [weak self] (_, value, _) in
+            self?.valueChangedSubject.onNext(value)
+        }
+    }
 
 }
--- UnderControlTests-macOS/Specs/ButtonSpec.swift
+++ UnderControlTests-macOS/Specs/ButtonSpec.swift
@@ -19,6 +19,86 @@ class ButtonSpec: QuickSpec {
             it("should have correct gcButton") {
                 expect(sut.gcButton).to(be(gcButton))
             }
+
+            context("value changed to 0.75") {
+                var scheduler: TestScheduler!
+                var observer: TestableObserver<Float>!
+
+                beforeEach {
+                    scheduler = TestScheduler(initialClock: 0)
+                    observer = scheduler.createObserver(Float.self)
+                    _ = sut.valueChanged.subscribe(observer)
+                    gcButton.valueChangedHandler?(gcButton, 0.75, true)
+                }
+
+                it("should produce one event") {
+                    expect(observer.events.count).to(equal(1))
+                }
+
+                it("should pass correct value") {
+                    expect(observer.events.first?.value.element).to(equal(0.75))
+                }
+            }
+
+            context("value changed to 0.25") {
+                var scheduler: TestScheduler!
+                var observer: TestableObserver<Float>!
+
+                beforeEach {
+                    scheduler = TestScheduler(initialClock: 0)
+                    observer = scheduler.createObserver(Float.self)
+                    _ = sut.valueChanged.subscribe(observer)
+                    gcButton.valueChangedHandler?(gcButton, 0.25, true)
+                }
+
+                it("should produce one event") {
+                    expect(observer.events.count).to(equal(1))
+                }
+
+                it("should pass correct value") {
+                    expect(observer.events.first?.value.element).to(equal(0.25))
+                }
+            }
+
+            context("pressed") {
+                var scheduler: TestScheduler!
+                var observer: TestableObserver<Bool>!
+
+                beforeEach {
+                    scheduler = TestScheduler(initialClock: 0)
+                    observer = scheduler.createObserver(Bool.self)
+                    _ = sut.pressedChanged.subscribe(observer)
+                    gcButton.pressedChangedHandler?(gcButton, 0.2, true)
+                }
+
+                it("should produce one event") {
+                    expect(observer.events.count).to(equal(1))
+                }
+
+                it("should pass correct value") {
+                    expect(observer.events.first?.value.element).to(beTrue())
+                }
+            }
+
+            context("depressed") {
+                var scheduler: TestScheduler!
+                var observer: TestableObserver<Bool>!
+
+                beforeEach {
+                    scheduler = TestScheduler(initialClock: 0)
+                    observer = scheduler.createObserver(Bool.self)
+                    _ = sut.pressedChanged.subscribe(observer)
+                    gcButton.pressedChangedHandler?(gcButton, 0, false)
+                }
+
+                it("should produce one event") {
+                    expect(observer.events.count).to(equal(1))
+                }
+
+                it("should pass correct value") {
+                    expect(observer.events.first?.value.element).to(beFalse())
+                }
+            }
         }
     }
 }
